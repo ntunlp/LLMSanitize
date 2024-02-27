c356502b48f9e3d3f2f5d85f0d79366bad5e785b
SparkJiao <jiaofangkai@hotmail.com>
2024-02-27 13:15:13 +0800

    - Add argument class.

--- lmsanitize/configs/config.py
+++ lmsanitize/configs/config.py
@@ -1,7 +1,8 @@
-
 from pathlib import Path
 import yaml
 from lmsanitize.utils.utils import dict_to_object
+from dataclasses import dataclass
+import torch
 
 # Read yaml config for variable configs (configuration that can change frequently)
 with open(Path(__file__).parent / 'main_config.yaml', 'r') as rf:
@@ -9,8 +10,39 @@ with open(Path(__file__).parent / 'main_config.yaml', 'r') as rf:
     config = dict_to_object(config_dict)
     print(config_dict)
 
-supported_methods = {dic['name']:dic for dic in config_dict['methods']}
+supported_methods = {dic['name']: dic for dic in config_dict['methods']}
 
 # Following are some constants (usually won't change)
 FAILURE_TOLERANCE = 2
 
+
+# Commented by Fangkai: We need a config class here to specify all default values of parameters and hide them from the config file,
+#  so that the user does not feel it complex.
+@dataclass
+class QueryArguments:
+    model_name: str = None
+    num_samples: int = 1
+    max_tokens: int = 128
+    top_logprobs: int = 0
+    max_request_time: int = 0
+    sleep_time: int = 0
+
+
+@dataclass
+class LocalModelArguments:
+    port: str = None
+    model_path: str = None
+    tokenizer_path: str = None
+    device: str = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+@dataclass
+class SanitizeArguments:
+    # openai
+    creds_key_file: str = None
+    # inference by local model
+    local: LocalModelArguments = LocalModelArguments()
+    # query parameters
+    query: QueryArguments = QueryArguments()
+
+# TODO: Add initialization for the argument classes above from the yaml config file.
--- lmsanitize/llm.py
+++ lmsanitize/llm.py
@@ -1,4 +1,8 @@
-from lmsanitize.utils.openai_api_utils import *
+from lmsanitize.utils.openai_api_utils import (
+    initialize_openai,
+    initialize_openai_local,
+    query_llm_api,
+)
 # from lmsanitize.configs.config import *
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
@@ -6,24 +10,43 @@ import torch
 
 
 class LLM:
-    def __init__(self, config, use_local_model=False):
+    def __init__(self, config):
+        """
+        :param config: config object
+            required fields:
+                For openai model initialization:
+                    - openai.creds_key_file
+                For vllm-based model initialization:
+                    - local.port
+                    - local.model_path
+                    - local.tokenizer_path
+                Request parameters:
+                    - query.model_name  # TODO: Maybe we can simply move these parameters to a parent field so that we can pass all of them at once.
+                    - query.num_samples
+                    - query.max_tokens
+                    - query.top_logprobs
+                    - query.max_request_time
+                    - query.sleep_time
+        """
         # Commented by Fangkai: we should put config as inputs or let the class be able to receive more parameters,
         #   so that we can initialize two models at the same time.
         self.config = config
-        if not use_local_model:
+        if config.openai:
+            assert config.openai.creds_key_file, "Please provide the path to your OpenAI API key."
             initialize_openai(config)
             self.query_fn = query_llm_api
             self.api_base = True
+        elif config.local.port:
+            assert config.local.port, "Please provide the url port to access your local model service."
+            initialize_openai_local(config)
+            self.query_fn = query_llm_api
+            self.config.openai.model_name = self.config.local.model_path
+            self.api_base = True
         else:
-            if self.config.api_base:
-                initialize_openai_local(config)
-                self.query_fn = query_llm_api
-                self.config.openai.model_name = self.config.local.model_path
-                self.api_base = True
-            else:
-                self.model = AutoModelForCausalLM.from_pretrained(self.config.local.model_path, torch_dtype=torch.float16).to(self.config.device)
-                self.tokenizer = AutoTokenizer.from_pretrained(self.config.local.model_path)
-                self.api_base = False
+            assert config.local.model_path and config.local.tokenizer_path, "Please provide the path to your local model and tokenizer."
+            self.model = AutoModelForCausalLM.from_pretrained(self.config.local.model_path, torch_dtype=torch.float16).to(self.config.device)
+            self.tokenizer = AutoTokenizer.from_pretrained(self.config.local.tokenizer_path)
+            self.api_base = False
 
     def query(self, prompt):
         if self.api_base:
