2f19a08722ace6ec5d9422682ffecd13b38f4a6d
Ravoxsg <mathieu.ravaut@gmail.com>
2024-03-26 22:55:14 +0800

    more scripts

--- scripts/tests/data/table_4/test_data_cont.sh
+++ scripts/tests/data/table_4/test_data_cont.sh
@@ -1,11 +1,14 @@
+n_points=100000
+method="platypus"
+
 # ARC-Challenge / validation
 python main.py \
 --dataset_name allenai/ai2_arc \
 --train_data_config_name "ARC-Challenge" \
 --eval_set_key validation \
 --text_keys "question+choices+answerKey" \
---n_eval_data_points 100 \
---method gpt-2
+--n_eval_data_points $n_points \
+--method $method
 
 # ARC-Challenge / test
 python main.py \
@@ -13,24 +16,24 @@ python main.py \
 --train_data_config_name "ARC-Challenge" \
 --eval_set_key test \
 --text_keys "question+choices+answerKey" \
---n_eval_data_points 100 \
---method gpt-2
+--n_eval_data_points $n_points \
+--method $method
 
 # HellaSwag / validation
 python main.py \
 --dataset_name Rowan/hellaswag \
 --eval_set_key validation \
 --text_keys "ctx+endings" \
---n_eval_data_points 100 \
---method gpt-2
+--n_eval_data_points $n_points \
+--method $method
 
 # HellaSwag / test
 python main.py \
 --dataset_name Rowan/hellaswag \
 --eval_set_key test \
 --text_keys "ctx+endings" \
---n_eval_data_points 100 \
---method gpt-2
+--n_eval_data_points $n_points \
+--method $method
 
 # Winogrande / validation
 python main.py \
@@ -38,8 +41,8 @@ python main.py \
 --train_data_config_name "winogrande_debiased" \
 --eval_set_key validation \
 --text_keys "sentence+option1+option2+answer" \
---n_eval_data_points 100 \
---method gpt-2
+--n_eval_data_points $n_points \
+--method $method
 
 # Winogrande / test
 python main.py \
@@ -47,8 +50,8 @@ python main.py \
 --train_data_config_name "winogrande_debiased" \
 --eval_set_key test \
 --text_keys "sentence+option1+option2+answer" \
---n_eval_data_points 100 \
---method gpt-2
+--n_eval_data_points $n_points \
+--method $method
 
 # GSM8K / test
 python main.py \
@@ -56,5 +59,5 @@ python main.py \
 --train_data_config_name "main" \
 --eval_set_key test \
 --text_keys "question+answer" \
---n_eval_data_points 100 \
---method gpt-2
+--n_eval_data_points $n_points \
+--method $method
