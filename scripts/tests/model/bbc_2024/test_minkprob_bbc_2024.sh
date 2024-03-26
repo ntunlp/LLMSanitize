dbe16d034e3f0cf3fbec4f028d15a89b7a67ca6d
Ravoxsg <mathieu.ravaut@gmail.com>
2024-03-26 23:43:41 +0800

    base

--- scripts/tests/model/bbc_2024/test_minkprob_bbc_2024.sh
+++ scripts/tests/model/bbc_2024/test_minkprob_bbc_2024.sh
@@ -1,9 +1,13 @@
+llama="/home/fangkai/pretrained-models/Llama-2-7b-chat-hf"
+mistralins="/home/fangkai/pretrained-models/Mistral-7B-Instruct-v0.2"
+qwen="/home/fangkai/pretrained-models/Qwen1.5-7B-Chat"
+
 python main.py \
 --method min-prob \
 --local_port 6000 \
 --local_port_2 6000 \
---model_name llama \
---model_name_2 llama \
+--model_name $llama \
+--model_name_2 $llama \
 --top_logprobs 5 \
 --max_tokens 1 \
 --echo \
@@ -11,7 +15,7 @@ python main.py \
 --eval_data_config_name "2024-02" \
 --eval_set_key train \
 --text_keys "description+section+content" \
---n_eval_data_points 100 \
+--n_eval_data_points 10 \
 --max_request_time 10 \
 --num_proc 64 \
 --output_dir ./output_dir/min_prob/llama2/bbc-2024
@@ -20,8 +24,8 @@ python main.py \
 --method min-prob \
 --local_port 6000 \
 --local_port_2 6000 \
---model_name mistral-ins \
---model_name_2 mistral-ins \
+--model_name $mistralins \
+--model_name_2 $mistralins \
 --top_logprobs 5 \
 --max_tokens 1 \
 --echo \
@@ -29,7 +33,7 @@ python main.py \
 --eval_data_config_name "2024-02" \
 --eval_set_key train \
 --text_keys "description+section+content" \
---n_eval_data_points 100 \
+--n_eval_data_points 10 \
 --max_request_time 10 \
 --num_proc 64 \
 --output_dir ./output_dir/min_prob/mistral-ins/bbc-2024
@@ -38,8 +42,8 @@ python main.py \
 --method min-prob \
 --local_port 6000 \
 --local_port_2 6000 \
---model_name qwen \
---model_name_2 qwen \
+--model_name $qwen \
+--model_name_2 $qwen \
 --top_logprobs 5 \
 --max_tokens 1 \
 --echo \
@@ -47,7 +51,7 @@ python main.py \
 --eval_data_config_name "2024-02" \
 --eval_set_key train \
 --text_keys "description+section+content" \
---n_eval_data_points 100 \
+--n_eval_data_points 10 \
 --max_request_time 10 \
 --num_proc 64 \
 --output_dir ./output_dir/min_prob/qwen/bbc-2024
--- scripts/vllm_hosting.sh
+++ scripts/vllm_hosting.sh
@@ -8,10 +8,10 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3;
 server_type=vllm.entrypoints.openai.api_server
 
 python -m $server_type \
-    --model /home/fangkai/pretrained-models/Mistral-7B-v0.1 \
-    --tokenizer /home/fangkai/pretrained-models/Mistral-7B-v0.1 \
+    --model /home/fangkai/pretrained-models/Llama-2-7b-chat-hf \
+    --tokenizer /home/fangkai/pretrained-models/Llama-2-7b-chat-hf \
     --gpu-memory-utilization=0.9 \
     --max-num-seqs=200 \
     --disable-log-requests \
-    --host 127.0.0.1 --port 6001 --tensor-parallel-size 4 \
+    --host 127.0.0.1 --port 6000 --tensor-parallel-size 4 \
     --download-dir /export/home/cache 
