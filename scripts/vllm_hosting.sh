7b07c0f13d9835f1fa9f9b7d818f0374fd00f576
Ravoxsg <mathieu.ravaut@gmail.com>
2024-03-27 00:35:56 +0800

    base

--- scripts/vllm_hosting.sh
+++ scripts/vllm_hosting.sh
@@ -4,14 +4,14 @@
 # Run this script in one tab first, then run the script calling the method in another tab
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 export RAY_memory_monitor_refresh_ms=0;
-export CUDA_VISIBLE_DEVICES=0,1,2,3;
+export CUDA_VISIBLE_DEVICES=0,1;
 server_type=vllm.entrypoints.openai.api_server
 
 python -m $server_type \
-    --model /home/fangkai/pretrained-models/Llama-2-7b-chat-hf \
-    --tokenizer /home/fangkai/pretrained-models/Llama-2-7b-chat-hf \
+    --model /home/fangkai/pretrained-models/Qwen1.5-7B-Chat \
+    --tokenizer /home/fangkai/pretrained-models/Qwen1.5-7B-Chat \
     --gpu-memory-utilization=0.9 \
     --max-num-seqs=200 \
     --disable-log-requests \
-    --host 127.0.0.1 --port 6000 --tensor-parallel-size 4 \
+    --host 127.0.0.1 --port 6000 --tensor-parallel-size 2 \
     --download-dir /export/home/cache 
