5c7529c6da7fb6fc4c6f17866a10e9bf9584e9bc
Ravoxsg <mathieu.ravaut@gmail.com>
2024-03-27 13:56:16 +0800

    passed sharded lkl test

--- scripts/tests/model/sharded-likelihood/test_bbc_2024.sh
+++ scripts/tests/model/sharded-likelihood/test_bbc_2024.sh
@@ -1,17 +1,12 @@
 # Get the options
-while getopts ":p:m:t:" option; do
+while getopts ":m:" option; do
    case $option in
-      p) # port number
-         port=$OPTARG;;
       m) # Enter model name
          model_name=$OPTARG;;
-      t) # local api type
-         local_api_type=$OPTARG;;
    esac
 done
 
 echo "model name ", $model_name
-echo "local port: ", $port
 
 # test min-K-prob model contamination method
 python main.py \
@@ -22,7 +17,6 @@ python main.py \
 --n_eval_data_points 100 \
 --num_proc 0 \
 --method sharded-likelihood \
---local_port $port \
 --model_name $model_name \
 --sharded_likelihood_context_len 1024 \
 --sharded_likelihood_stride 512 \
